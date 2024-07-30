38fa472ee6d5e21ad076d97d4e872ee1694fe960
ssssss <waseeqsaad@gmail.com>
2024-07-30 18:04:03 +0500

    Update models file with new changes

--- app/models.py
+++ app/models.py
@@ -6,24 +6,24 @@ from sqlalchemy.orm import relationship
 
 
 class Post(Base):
-    __tablename__="posts"
+    __tablename__="Posts"
     id=Column(Integer,primary_key=True,nullable=False)
     title=Column(String,nullable=False)
     content=Column(String,nullable=False)
     published=Column(Boolean,server_default="TRUE",nullable=False)
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("now()"))
-    owner_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
+    owner_id=Column(Integer,ForeignKey("Users.id",ondelete="CASCADE"),nullable=False)
     owner=relationship("User")
     
 class User(Base):
-    __tablename__="users"
+    __tablename__="Users"
     id=Column(Integer,primary_key=True,nullable=False)
     email=Column(String,nullable=False,unique=True)
     password=Column(String,nullable=False)
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("now()"))
     
 class Vote(Base):
-    __tablename__="votes"
-    post_id=Column(Integer,ForeignKey("posts.id",ondelete="CASCADE"),primary_key=True,nullable=False)
-    user_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True,nullable=False)
+    __tablename__="Votes"
+    post_id=Column(Integer,ForeignKey("Posts.id",ondelete="CASCADE"),primary_key=True,nullable=False)
+    user_id=Column(Integer,ForeignKey("Users.id",ondelete="CASCADE"),primary_key=True,nullable=False)
     
