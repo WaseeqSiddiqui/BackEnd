cde507453f70ad5b8f2d5481c957087fd312fc40
ssssss <waseeqsaad@gmail.com>
2024-07-30 15:50:43 +0500

    remove procfile

--- app/routers/votes.py
+++ app/routers/votes.py
@@ -17,7 +17,7 @@ def vote(vote:schemas.Vote,db:Session=Depends(get_db),current_user:int=Depends(o
     if vote.dir==1:
         if found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                detail=f"user {current_user.id} has already voted on post{vote.post_id}")  
+                                detail=f"user {current_user.id} has already voted on post {vote.post_id}")  
         new_vote=models.Vote(post_id=vote.post_id,user_id=current_user.id)
         db.add(new_vote)
         db.commit()
